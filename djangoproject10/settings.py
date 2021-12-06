225a213cdbe3efa8c50e76cc22ae2f513497dc18
franciscowendel <cisco.wendel@gmail.com>
2021-12-06 17:29:41 -0300

    Guardando 'configs' importantes no arquivo  '.env' com o python decouple

--- djangoproject10/settings.py
+++ djangoproject10/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config, Csv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e_#(7g+*-3w(n)+d4-!48%gv=j5%#mj-+2gof&-08zdfa-xc3b'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
