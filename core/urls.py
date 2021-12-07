be947fd9c332daf03eb3e58b82cbd8bd3c437d99
franciscowendel <cisco.wendel@gmail.com>
2021-12-07 01:16:43 -0300

    'urls.py' do app 'core' configurada.

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, contact, product
 
 
-urlspatterns = [
+urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('product/', product, name='product'),
