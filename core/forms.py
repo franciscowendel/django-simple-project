48ac591a7d3b95a8b9e9309561ee8e7c580d788c
franciscowendel <cisco.wendel@gmail.com>
2021-12-07 00:51:11 -0300

    Formulário de contato/envio de email criado.

---
+++ core/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.core.mail import EmailMessage
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100)
+    email = forms.EmailField(label='Email', max_length=100)
+    subject = forms.CharField(label='Subject', max_length=150)
+    message = forms.CharField(label='Message', widget=forms.Textarea())
+
+    def send_mail(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        subject = self.cleaned_data['subject']
+        message = self.cleaned_data['message']
+
+        content = f'Name: {name}\nEmail: {email}\nSubject: {subject}\nMessage: {message}'
+
+        mail = EmailMessage(
+            subject=subject,
+            body=content,
+            from_email='youremail@yourdomain.com',
+            to=['test1@gmail.com', 'test2@gmail.com', ],
+            headers={'Reply-to': email},
+        )
+        mail.send()
