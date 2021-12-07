dbfdc583797db2c63af48b5fbc4b34d292d8800f
franciscowendel <cisco.wendel@gmail.com>
2021-12-07 00:14:22 -0300

    Model 'Base' criado.

--- core/models.py
+++ core/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 from stdimage.models import StdImageField
+
+
+class Base(models.Model):
+    created_at = models.DateField('Created', auto_now_add=True)
+    modified_at = models.DateField('Modified', auto_now=True)
+    active = models.BooleanField('Is Active?', default=True)
+
+    class Meta:
+        abstract = True
