ba5ec07e5e7ddd5cccfe52840ed8421c5be75553
franciscowendel <cisco.wendel@gmail.com>
2023-01-16 00:52:13 -0300

    Arquivo "urls.py" do app core editado. ProductAPIView adicionado na url

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import index, contact, product
+from .views import \
+    index, \
+    contact, \
+    product, \
+    ProductAPIView
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('product/', product, name='product'),
+
+    path('products/', ProductAPIView.as_view(), name='products'),
 ]
