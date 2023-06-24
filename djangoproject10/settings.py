c1597dc95b19da373b1c3be82418389098cf31da
Francisco <cisco.wendel@gmail.com>
2023-06-24 19:21:06 -0300

    Update settings.py
    Fixing error on REST_FRAMEWORK settings

--- djangoproject10/settings.py
+++ djangoproject10/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
-    'DEFAULT_PAGINATION_CLASSE': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
     'DEFAULT_THROTTLE_CLASSES': (
         'rest_framework.throttling.AnonRateThrottle',
