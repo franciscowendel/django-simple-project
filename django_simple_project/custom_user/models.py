890590c045e3ff27f40d79d5db3d5802584d9855
Francisco <cisco.wendel@gmail.com>
2024-01-12 23:17:21 -0300

    Update models.py

--- django_simple_project/custom_user/models.py
+++ django_simple_project/custom_user/models.py
@@ -64,7 +64,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["first_name"]
 
     class Meta:
         verbose_name = _('user')
